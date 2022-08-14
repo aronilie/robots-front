e8567b0cd4bda6051d7ce626bbcdd82ee3879d20
Aron Ilie <aronilie.code@gmail.com>
2022-08-14 13:22:02 +0200

    Created test for actionCreators

--- src/features/robots/reducer/robotsReducer.test.ts
+++ src/features/robots/reducer/robotsReducer.test.ts
@@ -2,11 +2,11 @@ import { loadRobotsAction } from "./actionCreators";
 import { robotsReducer } from "./robotsReducer";
 
 describe("Given a robotsReducer reducer", () => {
-  describe("When called with a getAll robots action as an argument", () => {
-    test("Then it should return an array with the tasks sent as a payload", () => {
+  describe("When called with loadRobotsAction as an argument", () => {
+    test("Then it should return an array with the robots sent as a payload", () => {
       const robots = [
         {
-          _id: "0",
+          _id: "",
           name: "",
           imgUrl: "",
           velocity: 0,
