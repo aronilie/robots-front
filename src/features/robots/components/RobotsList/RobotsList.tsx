c9aef06012915603336538eeed2d9135157fc9a1
Aron Ilie <aronilie.code@gmail.com>
2022-08-15 10:19:24 +0200

    Changed APIUrl

--- src/features/robots/components/RobotsList/RobotsList.tsx
+++ src/features/robots/components/RobotsList/RobotsList.tsx
@@ -12,7 +12,9 @@ const RobotsList = (): JSX.Element => {
 
   const dispatch = useDispatch();
 
+  console.log(process.env.REACT_APP_API_URL);
   const APIUrl = process.env.REACT_APP_API_URL as string;
+  console.log(process.env.REACT_APP_API_URL);
 
   const repositoryRobots = useMemo(
     () => new RestRepository<Robot>(APIUrl),
