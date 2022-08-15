02e0cc9ddb32a4b3f14072474f2e207ffa9d5f7b
Aron Ilie <aronilie.code@gmail.com>
2022-08-15 19:42:32 +0200

    Created actionCreator and actionType for createRobot action

--- src/features/robots/actionCreators/actionCreators.ts
+++ src/features/robots/actionCreators/actionCreators.ts
@@ -2,4 +2,6 @@ import { createAction } from "@reduxjs/toolkit";
 import Robot from "../models/Robots";
 import { actionTypes } from "../actionTypes/actionTypes";
 
-export const loadRobotsAction = createAction<Robot[]>(actionTypes.robotsLoad);
+export const loadRobotsAction = createAction<Robot[]>(actionTypes.loadRobots);
+
+export const createRobotAction = createAction<Robot>(actionTypes.createRobot);
