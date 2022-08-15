c0cdcfa53ab173625ac38507a87f4e644178fee2
Aron Ilie <aronilie.code@gmail.com>
2022-08-15 20:14:26 +0200

    Tested case createRobotAction from reducer

--- src/features/robots/reducer/robotsReducer.test.ts
+++ src/features/robots/reducer/robotsReducer.test.ts
@@ -1,4 +1,7 @@
-import { loadRobotsAction } from "../actionCreators/actionCreators";
+import {
+  createRobotAction,
+  loadRobotsAction,
+} from "../actionCreators/actionCreators";
 import { robotsReducer } from "./robotsReducer";
 
 describe("Given a robotsReducer reducer", () => {
@@ -19,6 +22,43 @@ describe("Given a robotsReducer reducer", () => {
       const result = robotsReducer(robots, action);
 
       expect(result).toStrictEqual(robots);
+    });
+  });
+  describe("When called with createRobotAction as an argument", () => {
+    test("Then it should return an array with a robot more", () => {
+      const robots = [
+        {
+          _id: "",
+          name: "",
+          imageUrl: "",
+          velocity: 0,
+          resistance: 0,
+          creationDate: "",
+        },
+        {
+          _id: "",
+          name: "",
+          imageUrl: "",
+          velocity: 0,
+          resistance: 0,
+          creationDate: "",
+        },
+      ];
+      const newRobot = {
+        _id: "a9df7a9",
+        name: "Terminator",
+        imageUrl: "terminatorimage.jpg",
+        velocity: 8,
+        resistance: 7,
+        creationDate: "25/03/2010",
+      };
+
+      const action = createRobotAction(newRobot);
+
+      const result = robotsReducer(robots, action);
+
+      expect(result).toHaveLength(3);
+      expect(result.includes(newRobot)).toBe(true);
     });
   });
 });
